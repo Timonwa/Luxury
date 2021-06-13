2ce6dcc8cd82ce7686d9b1024450660196e6bbf5
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-06-13 23:23:20 +0100

    made table in post responsive

--- js/index.js
+++ js/index.js
@@ -79,3 +79,56 @@ function prev() {
     }
     showImage(currentImage);
 };
+
+// ! ratings star
+// varibles
+let ratingNumber = document.querySelector(".rating-number");
+const star1 = document.querySelector(".rstar1");
+const star2 = document.querySelector(".rstar2");
+const star3 = document.querySelector(".rstar3");
+const star4 = document.querySelector(".rstar4");
+const star5 = document.querySelector(".rstar5");
+
+star1.addEventListener('click', function () {
+    if (star1.style.color == "var(--main-color6-inactive)") {
+        star1.style.color = "#107bff"
+        ratingNumber.innerHTML = "1";
+    }
+    else {
+        star1.style.color = "var(--main-color6-inactive)"
+        ratingNumber.innerHTML = "0";
+    }
+    // star1.classList.toggle("star-toggle");
+});
+star2.addEventListener('click', function () {
+    if (star2.style.color == "var(--main-color6-inactive)") {
+        star2.style.color = "#107bff"
+        ratingNumber.innerHTML = "2";
+    }
+    else {
+        star2.style.color = "var(--main-color6-inactive)"
+        ratingNumber.innerHTML = initial;
+    }
+});
+
+// for (i of ratingNumber) {
+//     i.textContent = "s";
+//     i.style.color = "red";
+// }
+
+// for (var p = 0; p < ratingNumber.length; p++) {
+//     ratingNumber[p].textContent = "p";
+//     ratingNumber[p].innerHTML = "g";
+// }
+
+// for (i of star1) {
+//     i.addEventListener('click', function () {
+//         if (this.style.color == "var(--main-color6-inactive)") {
+//             this.style.color = "#107bff";
+//             ratingNumber[i].innerHTML = "1";
+//         }
+//         else {
+//             this.style.color = "var(--main-color6-inactive)";
+//         };
+//     });
+// }
